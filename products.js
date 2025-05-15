f66f3877afb0f1aa45b9929dc60de3ad69829a26
Lilly-N1 <lino2028@tisbrain.net>
2025-05-15 21:28:02 +0900

    Delete products.js

--- products.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.item img').forEach(img => {
-  img.addEventListener('click', () => {
-    img.classList.add('clicked');
-    setTimeout(() => {
-      img.classList.remove('clicked');
-    }, 200); 
-  });
-});
